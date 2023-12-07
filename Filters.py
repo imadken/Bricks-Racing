1288622b29ed81ad9618f4a7d7587c6f8258875b
imadken <imad.2002@yahoo.fr>
2023-12-07 18:20:06 +0100

    fixed Erosion and dilatation, added OPEN and CLOSE

--- Filters.py
+++ Filters.py
@@ -14,67 +14,77 @@ class Filters():
     @staticmethod
     def filtreMoy(img, vois=7):
         
-        h, w = img.shape
-        imgtoy = np.zeros(img.shape, img.dtype)
+        img_height, img_width = img.shape
+        pad= vois // 2
         
-        y=0
+        #Add padding
+        img_copy = np.zeros((img_height + pad + pad, img_width + pad + pad), dtype=img.dtype)
+        img_copy[pad:pad+img_height, pad:pad+img_width] = img
         
-        while(y<h):
-            x=0
-            while(x<w):
-                if(y < (vois/2) or y > (h - vois/2) or x < (vois/2) or x > (w - vois/2)):
-                    imgtoy[y, x] = img[y, x]
+        img_result = np.zeros(img_copy.shape, img_copy.dtype)
+        
+        img_height_new = img_height+pad+pad
+        img_width_new = img_width+pad+pad
+
+        for y in range(img_height_new):   
+            for x in range(img_width_new):
+
+                if(y < (pad) or y > (img_height_new - pad) or x < (pad) or x > (img_width_new - pad)):
+                    img_result[y, x] = img_copy[y, x]
                 else:
-                    n = int(vois/2)
-                    imgvois = img[int(y - n):int(y + n + 1), int(x - n):int(x+n+1)]
+                    n = vois//2
+                    imgvois = img_copy[int(y - n):int(y + n + 1), int(x - n):int(x+n+1)]
                     # imgtoy[y, x] = np.mean(imgvois)
-                    imgtoy[y, x] = Metrics.mean(imgvois)
-                x+=1
-            y+=1
+                    img_result[y, x] = Metrics.mean(imgvois)
+               
             # if (y%10 == 0):
                 # print(y)
-                        
-        return imgtoy
+        #crop back to original
+        img_result = img_result[pad:pad+img_height, pad:pad+img_width]                
+        return img_result
     
     
     @staticmethod
     def filtreMed(img, vois=7):
         
-        h, w = img.shape
-        imgmed = np.zeros(img.shape, img.dtype)
+        img_height, img_width = img.shape
+        pad= vois // 2
         
-        y=0
+        #Add padding
+        img_copy = np.ones((img_height + pad + pad, img_width + pad + pad), dtype=img.dtype)
+        img_copy[pad:pad+img_height, pad:pad+img_width] = img
         
-        while(y<h):
-            x=0
-            while(x<w):
-                if(y < (vois/2) or y > (h - vois/2) or x < (vois/2) or x > (w - vois/2)):
-                    imgmed[y, x] = img[y, x]
+        img_result = np.ones(img_copy.shape, img_copy.dtype)
+        
+        img_height_new = img_height+pad+pad
+        img_width_new = img_width+pad+pad
+        
+    
+        for y in range(img_height_new):
+            for x in range(img_width_new):
+                if(y < pad or y > (img_height_new - pad) or x < pad or x > (img_width_new - pad)):
+                    img_result[y, x] = img_copy[y, x]
                 else:
-                    n = int(vois/2)
-                    imgvois = img[int(y - n):int(y + n + 1), int(x - n):int(x+n+1)]
-                    
+                    n = pad
+                    imgvois = img_copy[int(y - n):int(y + n + 1), int(x - n):int(x+n+1)]
+
                     #get voisinage
                     t = np.zeros((vois*vois), img.dtype)
                     hv,wv=imgvois.shape
-                    
-                    yv=0
-                    while yv < hv:
-                        xv=0
-                        while xv < wv:
+
+                    for yv in range(hv):
+                        for xv in range(wv):
                             # t[yv*vois + xv] = imgmed[yv, xv]
                             t[yv*vois + xv] = imgvois[yv, xv]
-                            xv += 1
-                        yv += 1
+                           
                         
-                    imgmed[y,x]= Metrics.median(t) 
-                      
-                x+=1
-            y+=1
+                    img_result[y,x]= Metrics.median(t) 
+               
             # if (y%10 == 0):
                 # print(y)
-                        
-        return imgmed
+        #crop back to original
+        img_result = img_result[pad:pad+img_height, pad:pad+img_width]                
+        return img_result
     
     @staticmethod
     def filter2D(img, kernel):
@@ -93,28 +103,25 @@ class Filters():
          # Calculate the padding needed for convolution
          pad_y = kernel_height // 2
          pad_x = kernel_width // 2
-     
+         
          # Create an empty output image
          img_result = np.zeros_like(img)
          
-         # Iterate over each pixel in the input image
-         y=pad_y
-         while y < img_height - pad_y:
-             x=pad_x
-             while x < img_width - pad_x :
+         for y in range(pad_y,img_height - pad_y):
+             for x in range (pad_x,img_width - pad_x) :
                  # Extract the local region around the pixel
                  img_region = img[y - pad_y:y + pad_y + 1, x - pad_x:x + pad_x + 1]
                  
                  # Perform the convolution
                  img_result[y, x] = Convolution.convolution(img_region,kernel)
-                 x += 1
-                 
-             y += 1 
+        
+         #crop back to original
+         img_result = img_result[pad_y:pad_y+img_height, pad_x:pad_x+img_width]
                  
          return img_result
      
     @staticmethod
-    def filter_Morph(img, kernel,type="EROSION",iterations=1):
+    def filter_Morph(img,type="EROSION",iterations=1,kernel_shape="RECT",size_kernel=1):
          """Performs either dilatation or erosion
  
          Args:
@@ -130,6 +137,7 @@ class Filters():
          else:
             method = Convolution.dilate
             
+         kernel = Convolution.create_structuring_element(kernel_shape,size_kernel)   
          
          img_height, img_width = img.shape
          kernel_height, kernel_width = kernel.shape
@@ -140,36 +148,72 @@ class Filters():
          # Calculate the padding needed for convolution
          pad_y = kernel_height // 2
          pad_x = kernel_width // 2
-         img_result = img.copy()
+         
         #  img_result = np.zeros_like(img)
+         img_result = img.copy()
+         img_copy = img.copy()
          # Iterate over each pixel in the input image
-         while iterations > 0:
-            y=pad_y
-            while y < img_height - pad_y:
-                x=pad_x
-                while x < img_width - pad_x :
+         for _ in range(iterations):
+            for y in range(pad_y,img_height - pad_y):
+                
+                for x in range(pad_x,img_width - pad_x):
                     # Extract the local region around the pixel
-                    img_region = img[y - pad_y:y + pad_y + 1, x - pad_x:x + pad_x + 1]
+                    img_region = img_copy[y - pad_y:y + pad_y + 1, x - pad_x:x + pad_x + 1]
                     
                     # Perform the convolution
                     img_result[y, x] = method(img_region,kernel)
+                  
                     
-                    x += 1  
-                y += 1
-                if y%10==0 :print(y)
-            
-            iterations -= 1 
-                
+                # if y%10==0 :print(y)
+            img_copy = img_result.copy()  
+               
          return img_result
      
+    @staticmethod 
+    def Open_Close_Morph(img,type="OPEN",iterations=1,kernel_shape="RECT",size_kernel=1):
+         """Performs either OPEN or CLOSE
+ 
+         Args:
+             img (ndarray): image tob eused
+             type (str, optional): OPEN or CLOSE. Defaults to "OPEN".
+             kernel_shape (ndarray): cross or rect
+             size_kernel (int): Defaults to 1
+ 
+         Returns:
+             ndarray: image after Filter
+         """
+
+         # Ensure the kernel has an odd size
+         assert size_kernel % 2 == 1, "Kernel size must be odd"
+
+        #  img_result = np.zeros_like(img)
+         img_result = img.copy()
+         img_copy = img.copy()
+         # Iterate over each pixel in the input image
+         for _ in range(iterations):
+          
+            if type.lower()=="open":
+               
+               img_result = Filters.filter_Morph(img_copy,"erosion",iterations=1,kernel_shape=kernel_shape,size_kernel=size_kernel)
+               img_result = Filters.filter_Morph(img_result,"dilate",iterations=1,kernel_shape=kernel_shape,size_kernel=size_kernel)
+               
+            else:
+               img_result = Filters.filter_Morph(img_copy,"dilate",iterations=1,kernel_shape=kernel_shape,size_kernel=size_kernel)
+               img_result = Filters.filter_Morph(img_result,"erosion",iterations=1,kernel_shape=kernel_shape,size_kernel=size_kernel)      
+              
+            img_copy = img_result.copy()  
+               
+         return img_result 
+     
     @staticmethod
-    def Sobel(img, Hkernel,Vkernel):
+    def Sobel(img,kernel_size=3):
          
-         """an implementation of CV2.filter2D for linear filters
+         """an implementation of Sobel filter
  
          Returns:
              result img: numpy array of the filtred input image 
          """
+         Hkernel,Vkernel=Metrics.generate_filter_kernel(filter_type="Sobel",size=kernel_size)
          
          img_copy = img.copy()
          img_copy=np.array(img_copy,dtype=np.float64)
@@ -177,51 +221,56 @@ class Filters():
          gradient_x = Filters.filter2D(img_copy,Hkernel)
          gradient_y = Filters.filter2D(img_copy,Vkernel)
          
-        #  gradient_x = Filters.filter2D(img,Hkernel)
-        #  gradient_y = Filters.filter2D(img,Vkernel)
-        #  gradient_x = cv2.filter2D(img, cv2.CV_64F, Hkernel)
-        #  gradient_y = cv2.filter2D(img, cv2.CV_64F, Vkernel)
-         
          magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
-        #  magnitude = np.uint8(magnitude)
-         
-         
-        #  magnitude = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-         
-                 
+
          return magnitude 
    
 if __name__=="__main__":
-    img = cv2.imread("barca.png", cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread("palestine.png", cv2.IMREAD_GRAYSCALE)
     
     
-    HSober_kernel = np.array([[-1, 0, 1],[ -2, 0, 2], [-1, 0, 1]])
-    VSober_kernel = np.array([[-1,-2,-1],[0,0,0],[1,2,1]]) 
-    emboss_kernel = np.array([[-2, -1, 0],[-1,  1, 1],[ 0,  1, 2]])
-    laplacien_kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
-    gauss_kernel = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])/16
-    erosion_kernel = Convolution.create_structuring_element(shape="rect",size=25)
+    # HSober_kernel = np.array([[-1, 0, 1],[ -2, 0, 2], [-1, 0, 1]])
+    # VSober_kernel = np.array([[-1,-2,-1],[0,0,0],[1,2,1]]) 
     
-    # s=time()
-    # img_res = Filters.filtreMoy(img)
-    # img_res = Filters.filtreMed(img)
+    HSober_kernel,VSober_kernel = Metrics.generate_filter_kernel(filter_type="Sobel",size=3)
+    
+    # emboss_kernel = np.array([[-2, -1, 0],[-1,  1, 1],[ 0,  1, 2]])
+    emboss_kernel = Metrics.generate_filter_kernel(filter_type="emboss",size=3)
+    
+    # laplacien_kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+    laplacien_kernel = Metrics.generate_filter_kernel(filter_type="laplacian",size=3)
+    
+    # gauss_kernel = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])/16
+    gauss_kernel = Metrics.generate_filter_kernel(filter_type="gaussian",size=3)
+    
+    # erosion_kernel = Convolution.create_structuring_element(shape="rect",size=1)
+    
+    # img_mean = Filters.filtreMoy(img)
+    # img_med = Filters.filtreMed(img)
     # img_laplacien = Filters.filter2D(img,laplacien_kernel)
-    img_emboss = Filters.filter2D(img,emboss_kernel)
+    # img_emboss = Filters.filter2D(img,emboss_kernel)
     # img_gauss = Filters.filter2D(img,gauss_kernel)
-    # img_binary = Convolution.threshold(img,128,255)
-    # img_erode = Filters.filter_Morph(img_binary,erosion_kernel,type="erosion")
-    # img_sobel = Filters.Sobel(img,HSober_kernel,VSober_kernel)
-    # e =time()
-    # print(e-s)
-    # cv2.imshow("mean",img_res)
+    img_binary = Convolution.threshold(img,128,255)
+    # img_erode = Filters.filter_Morph(img_binary,iterations=1,type="erosion",kernel_shape="RECT",size_kernel=1)
+    # img_dilate = Filters.filter_Morph(img_binary,iterations=1,type="dilate",kernel_shape="RECT",size_kernel=5)
+    # img_sobel = Filters.Sobel(img,kernel_size=3)
+    
+    img_close = Filters.Open_Close_Morph(img,type="close",iterations=1)
+
+    # cv2.imshow("before",img)
+    
+    # cv2.imshow("mean",img_mean)
+    # cv2.imshow("median",img_med)
     # cv2.imshow("median",img_res)
     # cv2.imshow("laplacien",img_laplacien)
-    # cv2.imshow("laplacien",img_gauss)
+    # cv2.imshow("gauss",img_gauss)
     # cv2.imshow("erode",img_erode)
-    # cv2.imshow("binary",img_binary)
-    cv2.imshow("before",img)
+    # cv2.imshow("dilate",img_dilate)
+    cv2.imshow("binary",img_binary)
     # cv2.imshow("sobel",img_sobel)
-    cv2.imshow("sobel",img_emboss)
+    cv2.imshow("close",img_close)
+    # cv2.imshow("emboss",img_emboss)
+
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- convolution.py
+++ convolution.py
@@ -5,15 +5,8 @@ class Convolution:
     def convolution(img,kernel):
         
         h,w=img.shape
-        sum_value = 0
-        
-        y=0
-        while y<h:
-            x=0
-            while x<w:
-                sum_value += img[y,x] * kernel[y,x]
-                x+=1
-            y+=1     
+        # sum_value = 0
+        sum_value = np.sum(img * kernel)
         return sum_value 
      
     @staticmethod
@@ -33,13 +26,11 @@ class Convolution:
          output=np.zeros_like(img)
          
          h, w = img.shape
-         y=0    
-         while y < h:
-             x=0
-             while x < w:
+
+         for y in range(h):   
+             for x in range(w):
                      output[y,x] = max_val if img[y,x] > thresh else 0  
-                     x += 1
-             y += 1        
+                          
          return output 
      
     @staticmethod 
@@ -47,8 +38,7 @@ class Convolution:
        # Ensure size is an odd number
        size = (size * 2) + 1
        # Create an empty matrix
-       
-       
+    
        if shape.lower() == "cross" :
                kernel = np.zeros((size, size), dtype=np.uint8)
                kernel[:, size // 2] = 1
@@ -62,38 +52,45 @@ class Convolution:
     @staticmethod
     def erode(img,kernel):
         h,w=img.shape
-        kernel_height, kernel_width = kernel.shape
+        # kernel_height, kernel_width = kernel.shape
         sum_value = 1
-        y=0
-        while y<h:
-            x=0
-            while x<w:
+        # sum_value = np.bitwise_and(img , kernel)
+        
+        for y in range(h):   
+             for x in range(w):
                 # if y != w // 2 and x != h // 2:
-                if y != kernel_height // 2 and x != kernel_width // 2:
-                   sum_value = img[y,x] & kernel[y,x] & sum_value
-                x+=1
-            y+=1     
-        return sum_value 
+                # if y != kernel_height // 2 and x != kernel_width // 2:
+                sum_value = img[y,x] & kernel[y,x] & sum_value
+        
+        if sum_value == 1 :
+            return 255           
+        return 0
     
+ 
     @staticmethod
     def dilate(img,kernel):
         h,w=img.shape
-        kernel_height, kernel_width = kernel.shape
+        # kernel_height, kernel_width = kernel.shape
         sum_value = 0
-        y=0
-        while y<h:
-            x=0
-            while x<w:
-                if y != kernel_height // 2 and x != kernel_width // 2:
-                   sum_value = img[y,x] | kernel[y,x] | sum_value
-                x+=1
-            y+=1     
-        return sum_value                   
-  
+        # sum_value = np.bitwise_or(img , kernel)
+        for y in range(h):   
+             for x in range(w):
+                # if y != kernel_height // 2 and x != kernel_width // 2:
+                sum_value = img[y,x] | kernel[y,x] | sum_value
+        if sum_value == 1 :
+            return 0          
+        return 255
+        # return sum_value                       
+
 if __name__=="__main__":
-    img = cv2.imread("palestine.png", cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread("palestine.png", cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread("palestine.png",cv2.IMREAD_COLOR)
+    if len(img.shape) == 3:
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    img_res = Convolution.threshold(img,128,255)
+    # img_res = Convolution.threshold(img,128,255)
+    
+    img_res = Convolution.color_detection(img,np.array([255,0,0]),np.array([170,30,30]))
   
     cv2.imshow("before",img)
     cv2.imshow("after",img_res)
